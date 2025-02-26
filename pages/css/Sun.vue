e3534989868e28e303679b4e577bfb4a3bf09700
yufengjie <2457522528@qq.com>
2025-02-26 09:45:31 +0800

    css sun

---
+++ pages/css/Sun.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="w-full min-h-100vh flex flex-wrap justify-center items-center bg-black">
+    <Sun class="w-300px h-400px overflow-hidden" />
+    <Sun class="w-300px h-400px overflow-hidden" base-color="#0984e3" light-color="#6c5ce7" :light-num="4" />
+    <Sun class="w-300px h-400px overflow-hidden" base-color="#00cec9" light-color="#fdcb6e" :light-num="8" />
+    <Sun class="w-300px h-400px overflow-hidden" base-color="#1dd1a1" light-color="#ff9ff3" :light-num="12" />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="flex gap-10px">
+    <NuxtLink to="css/flipcardwall">
+      css flip wall
+    </NuxtLink>
+    <NuxtLink to="css/sun">
+      css animate sun
+    </NuxtLink>
+  </div>
+</template>
+
+<style>
+
+</style>
