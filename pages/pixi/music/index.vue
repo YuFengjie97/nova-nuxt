95a6b062323f11f5f76cc6d68cdee36426c1548a
yufengjie <2457522528@qq.com>
2025-03-16 21:03:01 +0800

    feat: chroma

--- pages/pixi/music/index.vue
+++ pages/pixi/music/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import chroma from 'chroma-js'
 import { Application } from 'pixi.js'
 import { BarChart, button, cirle, LineChart } from './ui'
 import music from '/sound/savageLove.aac'
@@ -7,6 +8,9 @@ const pixiCon = ref<HTMLElement>()
 const app = new Application()
 const fftSize = 1024
 const dataLength = 100
+const frequencyColors = chroma.scale(['#fdcb6e', '#6c5ce7'])
+  .mode('lch')
+  .colors(7)
 
 async function initApp(app: Application, container: HTMLElement) {
   await app.init({ antialias: true, background: '#1099bb', resizeTo: container })
@@ -33,13 +37,13 @@ onMounted(async () => {
   const timeDomainChart = new LineChart(app, { w: 800, h: 200 }, dataSize)
   timeDomainChart.setPos(80, 400)
 
-  const c1 = cirle(app).setPos(40, 240).setStyle (20, '#fdcb6e')
-  const c2 = cirle(app).setPos(80, 240).setStyle(20, '#fdcb6e')
-  const c3 = cirle(app).setPos(120, 240).setStyle(20, '#fdcb6e')
-  const c4 = cirle(app).setPos(160, 240).setStyle(20, '#fdcb6e')
-  const c5 = cirle(app).setPos(200, 240).setStyle(20, '#fdcb6e')
-  const c6 = cirle(app).setPos(240, 240).setStyle(20, '#fdcb6e')
-  const c7 = cirle(app).setPos(280, 240).setStyle(20, '#fdcb6e')
+  const c1 = cirle(app).setPos(40, 240).setStyle (20, frequencyColors[0])
+  const c2 = cirle(app).setPos(80, 240).setStyle(20, frequencyColors[1])
+  const c3 = cirle(app).setPos(120, 240).setStyle(20, frequencyColors[2])
+  const c4 = cirle(app).setPos(160, 240).setStyle(20, frequencyColors[3])
+  const c5 = cirle(app).setPos(200, 240).setStyle(20, frequencyColors[4])
+  const c6 = cirle(app).setPos(240, 240).setStyle(20, frequencyColors[5])
+  const c7 = cirle(app).setPos(280, 240).setStyle(20, frequencyColors[6])
 
   app.ticker.add(() => {
     if (!audio.paused) {
