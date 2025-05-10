bf3e51706d4445364314a005be9392ca03b5d5b5
yufengjie <2457522528@qq.com>
2025-05-10 09:21:31 +0800

    1

--- shaderToy/rect_cos.glsl
+++ shaderToy/rect_cos.glsl
@@ -5,25 +5,27 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     uv *= 4.;
 
     vec3 col = vec3(0.);
-    
-    float d = length(cos(uv));
-    
-    float aa = fwidth(d);
-    
-    float r = .96;    
-    float shape = smoothstep(aa+r,r,d);
-    col += shape;
-
     vec3 red = vec3(1.,0.,0.);
     vec3 gre = vec3(0.,1.,0.);
-    for(float i=-2.;i<=2.;i++){
+    vec3 blu = vec3(0.,0.,1.);
+    
+    vec2 u = cos(uv);
+    //u = max(u, u.yx);
+    float d = length(u);
+    float aa = fwidth(d);
+    
+    float r = .99;    
+    float shape = smoothstep(aa+r,r,d);
+    col += shape*blu;
+    
+    for(float i=-2.;i<=2.;i+=0.5){
       float l = cos(uv.x) + (floor(uv.y)+i) - uv.y;
       float ld = smoothstep(0.1,0.,abs(l));
-      col = mix(col, red, ld);
+      col += ld*red;
 
       float l2 = cos(uv.y) + (floor(uv.x)+i) - uv.x;
       float ld2 = smoothstep(0.1,0.,abs(l2));
-      col = mix(col, gre, ld2);
+      col += ld2 * gre;
     }
     
 
