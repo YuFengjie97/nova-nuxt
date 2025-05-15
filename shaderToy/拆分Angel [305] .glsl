1c5f24cb7e3edf24ad15e0f5a9f91c96f9365dfd
yufengjie <2457522528@qq.com>
2025-05-15 20:55:45 +0800

    1

--- shaderToy/拆分Angel [305] .glsl
+++ shaderToy/拆分Angel [305] .glsl
@@ -11,6 +11,11 @@ mat2 rotate(float a){
   return mat2(c,-s,s,c);
 }
 
+// https://easings.net/#easeOutElastic
+float easeOutElastic(float x) {
+  return clamp(0.,1.,pow(2., -10. * x) * sin((x * 10. - 0.75) * PI*0.3) +0.7);
+}
+
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
   vec2 uv = (I*2.-R)/R.y;
@@ -23,7 +28,7 @@ void mainImage(out vec4 O, in vec2 I){
   O.a = 1.;
 
   // todo 测试ro z为0
-  vec3 ro = vec3(0.,0.,-1.);
+  vec3 ro = vec3(0.,0.,0.);
   vec3 rd = normalize(vec3(uv, 1.));
 
   float z = 0.;
@@ -32,7 +37,7 @@ void mainImage(out vec4 O, in vec2 I){
   for(float i=0.;i<50.;i++){
     vec3 p = rd * z + ro;
 
-    p.z += -15.;
+    p.z += -16.;
     p.xz *= rotate(iTime * 0.2);
 
     if(iMouse.z > 0.) {
@@ -44,12 +49,13 @@ void mainImage(out vec4 O, in vec2 I){
     // amp 累积过大,会让颜色(形状)出现过多的噪音
     float amp = 1.;
     for(float i=1.;i<15.;i++){
-      p += amp * cos(p.zxy * freq + T * 0.1) / freq;
-      freq *= 2. + sin(T)*0.5;
+      p += amp * sin(p.zxy * freq + T * 0.1) / freq;
+      // freq *= 1.3 + easeOutElastic(fract(T*0.6)) * 0.8;
+      freq *= 1.5 + .8 * (sin(T) * 0.5 + 0.5);
       amp *= 1.;
     }
 
-    // xyz本质是圆
+    // xyz是圆
     // xy,xz,yz,是线
     // x,y,z是面
     d = .01 + abs(length(p.xyz) - 5.) / 10.;
