d2170a669aa1dbf22d46d0bb1335f9c609a9e328
yufengjie <2457522528@qq.com>
2025-05-17 16:00:55 +0800

    噪音们

---
+++ shaderToy/几种噪音下的fbm.glsl
@@ -0,0 +1,164 @@
+float random (in vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))
+                 * 43758.5453123);
+}
+float valueNoise (in vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f*f*(3.0-2.0*f);
+    // u = smoothstep(0.,1.,f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+vec2 random2(vec2 st){
+    st = vec2( dot(st,vec2(127.1,311.7)),
+              dot(st,vec2(269.5,183.3)) );
+    return -1.0 + 2.0*fract(sin(st)*43758.5453123);
+}
+float gradientNoise(vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    vec2 u = f*f*(3.0-2.0*f);
+
+    return mix( mix( dot( random2(i + vec2(0.0,0.0) ), f - vec2(0.0,0.0) ),
+                     dot( random2(i + vec2(1.0,0.0) ), f - vec2(1.0,0.0) ), u.x),
+                mix( dot( random2(i + vec2(0.0,1.0) ), f - vec2(0.0,1.0) ),
+                     dot( random2(i + vec2(1.0,1.0) ), f - vec2(1.0,1.0) ), u.x), u.y);
+}
+
+
+
+// Some useful functions
+vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
+
+float simplexNoise(vec2 v) {
+
+    // Precompute values for skewed triangular grid
+    const vec4 C = vec4(0.211324865405187,
+                        // (3.0-sqrt(3.0))/6.0
+                        0.366025403784439,
+                        // 0.5*(sqrt(3.0)-1.0)
+                        -0.577350269189626,
+                        // -1.0 + 2.0 * C.x
+                        0.024390243902439);
+                        // 1.0 / 41.0
+
+    // First corner (x0)
+    vec2 i  = floor(v + dot(v, C.yy));
+    vec2 x0 = v - i + dot(i, C.xx);
+
+    // Other two corners (x1, x2)
+    vec2 i1 = vec2(0.0);
+    i1 = (x0.x > x0.y)? vec2(1.0, 0.0):vec2(0.0, 1.0);
+    vec2 x1 = x0.xy + C.xx - i1;
+    vec2 x2 = x0.xy + C.zz;
+
+    // Do some permutations to avoid
+    // truncation effects in permutation
+    i = mod289(i);
+    vec3 p = permute(
+            permute( i.y + vec3(0.0, i1.y, 1.0))
+                + i.x + vec3(0.0, i1.x, 1.0 ));
+
+    vec3 m = max(0.5 - vec3(
+                        dot(x0,x0),
+                        dot(x1,x1),
+                        dot(x2,x2)
+                        ), 0.0);
+
+    m = m*m ;
+    m = m*m ;
+
+    // Gradients:
+    //  41 pts uniformly over a line, mapped onto a diamond
+    //  The ring size 17*17 = 289 is close to a multiple
+    //      of 41 (41*7 = 287)
+
+    vec3 x = 2.0 * fract(p * C.www) - 1.0;
+    vec3 h = abs(x) - 0.5;
+    vec3 ox = floor(x + 0.5);
+    vec3 a0 = x - ox;
+
+    // Normalise gradients implicitly by scaling m
+    // Approximation of: m *= inversesqrt(a0*a0 + h*h);
+    m *= 1.79284291400159 - 0.85373472095314 * (a0*a0+h*h);
+
+    // Compute final noise value at P
+    vec3 g = vec3(0.0);
+    g.x  = a0.x  * x0.x  + h.x  * x0.y;
+    g.yz = a0.yz * vec2(x1.x,x2.x) + h.yz * vec2(x1.y,x2.y);
+    return 130.0 * dot(m, g);
+}
+
+
+#define OCTAVES 6
+float fbm (in vec2 st, int type) {
+    // Initial values
+    float value = 0.0;
+    float amplitude = .5;
+    float frequency = 1.;
+    //
+    // Loop of octaves
+    for (int i = 0; i < OCTAVES; i++) {
+        if(type == 0) {
+            value += amplitude * valueNoise(st * frequency);
+        }
+        if(type == 1){
+            value += amplitude * gradientNoise(st * frequency);
+        }
+        if(type == 2){
+            value += amplitude * simplexNoise(st * frequency);
+        }
+        frequency *= 2.;
+        amplitude *= .5;
+    }
+    return value;
+}
+
+void mainImage(out vec4 O, in vec2 I){
+    vec2 R = iResolution.xy;
+    vec2 uv = I/R.xy;
+    O.rgb *= 0.;
+    O.a = 1.;
+
+    uv *= vec2(3.,2.);
+    vec2 i = floor(uv);
+    vec2 f = fract(uv)  * 10.;
+
+    float n = 0.;
+    if(i.x==0. && i.y==0.){
+        n = valueNoise(f);
+    }
+    if(i.x==1. && i.y==0.){
+        n = gradientNoise(f);
+    }
+    if(i.x==2. && i.y==0.){
+        n = simplexNoise(f);
+    }
+
+    if(i.x==0. && i.y==1.){
+        n = fbm(f, 0);
+    }
+    if(i.x==1. && i.y==1.){
+        n = fbm(f, 1);
+    }
+    if(i.x==2. && i.y==1.){
+        n = fbm(f, 2);
+    }
+
+
+    O.rgb = vec3(n);
+}
