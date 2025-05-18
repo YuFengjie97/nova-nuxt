2c8c4b21181b0574298f8cea59bdfa043300a496
yufengjie <2457522528@qq.com>
2025-05-18 19:51:20 +0800

    用两个噪音来遮盖交界处

--- shaderToy/边缘噪音_环形烟雾_角度越界噪音不连续问题.glsl
+++ shaderToy/边缘噪音_环形烟雾_角度越界噪音不连续问题.glsl
@@ -64,7 +64,6 @@ float getBlenderNoise(vec2 p, int type){
   }
   if(type == 1) {
     float nn = fbm(p);
-    nn += pow(nn,1.6);
     n += nn;
   }
 
@@ -74,39 +73,50 @@ float getBlenderNoise(vec2 p, int type){
 
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
+
   vec2 uv = (I*2.-R)/R.y;
+  
   float T = iTime * 6e-3;
   O.rgb *= 0.;
   O.a = 1.;
 
   O.rgb += grid(uv);
 
-  vec2 q = vec2(atan(uv.y, uv.x) + PI, (length(uv)));
-  q.x = sin(q.x);
+  vec2 q = vec2(atan(uv.y, uv.x), length(uv));
+  vec2 q2= vec2(atan(uv.y,-uv.x), length(uv));
 
   float scale = 0.02;
-  vec2 offset = vec2(0., -T);
+  vec2 offset = vec2(0., -T)*0.9;
   q *= scale;
   q += offset;
+  q2 *= scale;
+  q2 += offset;
 
-
-  float n = 0.;
-  if(iMouse.z > 0.){
-    n += getBlenderNoise(q, 0);
-  }else{
-    n += getBlenderNoise(q, 1);
-  }
 
   float r = 0.4;
   float d = length(uv);
-  float w = 0.4;
+  float w = 0.3;
   float s = smoothstep(w,0.,abs(d-r));
+
+  float n = 0.;
+  float nR = 0.;
+  float nL = 0.;
+  if(iMouse.z > 0.){
+    nR = getBlenderNoise(q, 0) * smoothstep(-r,0.,uv.x);
+    nL = getBlenderNoise(q2+vec2(.1,.2), 0) * smoothstep(r,0.,uv.x);
+  }else{
+    nR = getBlenderNoise(q, 1) * smoothstep(-r,0.,uv.x);
+    nL = getBlenderNoise(q2+vec2(.1,.2), 1) * smoothstep(r,0.,uv.x);
+  }
+  n = max(nR, nL);
+  n *= 2. + n * 4.; // 补强特征
+
 
   s *= n;
   float v = 0.5;       // 确定哪些值会表现形状的阈值,0.5取一半的值,按理是表现最好的
-  float feath = 0.1;  // 值越大,噪音的边缘越模糊
+  float feath = 0.3;   // 值越大,噪音的边缘越模糊
 
-  vec3 c = sin(vec3(3.,2.,1.) + pow(s, 4.)*2.5);
+  vec3 c = sin(vec3(.1,1.,1.) + s);
 
   s = smoothstep(v, v+feath, s);
 
