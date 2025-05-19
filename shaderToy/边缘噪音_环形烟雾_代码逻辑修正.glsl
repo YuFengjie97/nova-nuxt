0d5d1c7788e60e2d9fe97a478ef1b9bf01f3046d
yufengjie <2457522528@qq.com>
2025-05-19 14:31:05 +0800

    发光

--- shaderToy/边缘噪音_环形烟雾_代码逻辑修正.glsl
+++ shaderToy/边缘噪音_环形烟雾_代码逻辑修正.glsl
@@ -1,14 +1,17 @@
 #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/noise_1.png"
 
 #define PI 3.141596
-#define BaseRadius 0.4
-#define NoiseRadius .3
+#define BaseRadius 0.5
+#define NoiseRadius .8
+#define scale vec2(0.02)
+#define offset vec2(0., -iTime * 5e-3)
+
 
 float fbm(vec2 p){
   float f = 1.;
   float a = .5;
   float n = 0.;
-  for(float i=0.;i<8.;i++){
+  for(float i=0.;i<4.;i++){
     n += a * texture(iChannel0, p * f).r;
     f *= 2.;
     a *= .5;
@@ -30,43 +33,30 @@ float fbm(vec2 p){
   // return t;
 }
 
-vec3 grid(vec2 p){
-  vec3 red = vec3(1.,0.,0.);
-  vec3 green = vec3(0.,1.,0.);
 
-  p = fract(p);
-  float d = min(abs(p.x), abs(p.y));
-  float aa = 0.01;
-  float s1 = smoothstep(0.02+aa,0.02,d);
-  vec3 col = s1 * red;
+vec2 twistUV(vec2 p){
+  p *= scale;
+  p += offset;
+  return p;
+}
 
-  p *= 5.;
-  p = fract(p);
-  float d2 = min(abs(p.x), abs(p.y));
-  float aa2 = 0.01;
-  float s2 = smoothstep(0.02+aa,0.02,d2);
-  col = mix(col, green, s2);
-
-  return col;
+float domainWraping(vec2 p){
+  // return fbm(twistUV(p));
+  return fbm(twistUV(p + fbm(twistUV(p))));
 }
 
 
-float getBlenderNoise(vec2 uv, vec2 scale, vec2 offset){
+float getBlenderNoise(vec2 uv){
   vec2 q = vec2(atan(uv.y, uv.x), (length(uv)));
   vec2 q2= vec2(atan(uv.y,-uv.x), (length(uv)));
 
-  q *= scale;
-  q += offset;
-  q2 *= scale;
-  q2 += offset;
+  float nR = domainWraping(q);
+  nR *= smoothstep(-BaseRadius,0.,uv.x);
 
-  float grad = smoothstep(NoiseRadius,0.,q.x);
+  float nL = domainWraping(q2+vec2(.1,.2));
+  nL *= smoothstep(BaseRadius,0.,uv.x);
 
-  float n = 0.;
-  float nR = fbm(q) * smoothstep(-BaseRadius,0.,uv.x);
-  float nL = fbm(q2+vec2(.1,.2)) * smoothstep(BaseRadius,0.,uv.x);
-  n = max(nR, nL);
-  n *= 2. + n * 4.; // 补强特征
+  float n = max(nR, nL);
 
   return n;
 }
@@ -77,28 +67,26 @@ void mainImage(out vec4 O, in vec2 I){
 
   vec2 uv = (I*2.-R)/R.y;
   
-  float T = iTime * 6e-3;
   O.rgb *= 0.;
   O.a = 1.;
-
-  // O.rgb += grid(uv);
 
 
   float d = length(uv);
   float s = smoothstep(NoiseRadius,0.,abs(d-BaseRadius));
 
-
-  vec2 scale = vec2(0.02);
-  vec2 offset = vec2(0., -T)*0.9;
-  float n = getBlenderNoise(uv, scale, offset);
-
+  float n = getBlenderNoise(uv);
 
   s *= n;
+  
   float v = 0.5;
-  float feath = 0.1;
+  float feath = .1;
+  s = smoothstep(v, v+feath, s); // 噪音的伪距离场
 
-  vec3 c = sin(vec3(3.,2.,1.) + s);
 
-  s = smoothstep(v, v+feath, s);
+  vec3 c_o = vec3(1.,0.,0.);
+  vec3 c_i = vec3(0.,1.,0.);
+  float glow = pow(1./s, 2.);  // 伪距离场发光
+  vec3 c = sin(vec3(3.,2.,1.) + n) * glow;
+
   O.rgb = mix(O.rgb, c, s);
 }
