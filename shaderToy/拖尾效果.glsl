ba5fd6700dc309d9aa7f2ed9c2a7f5d72d6a57f4
yufengjie <2457522528@qq.com>
2025-05-30 14:22:55 +0800

    拖尾

---
+++ shaderToy/拖尾效果.glsl
@@ -0,0 +1,62 @@
+#define T iTime
+#define PI 3.141596
+
+vec2 hash(vec2 p) {
+    p = vec2(dot(p, vec2(127.1, 311.7)),
+             dot(p, vec2(269.5, 183.3)));
+    return -1.0 + 2.0 * fract(sin(p) * 43758.5453123);
+}
+float noise(vec2 p) {
+    const float K1 = 0.366025404; // (sqrt(3)-1)/2
+    const float K2 = 0.211324865; // (3-sqrt(3))/6
+
+    vec2 i = floor(p + (p.x + p.y) * K1);
+    vec2 a = p - i + (i.x + i.y) * K2;
+    vec2 o = (a.x > a.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+    vec2 b = a - o + K2;
+    vec2 c = a - 1.0 + 2.0 * K2;
+
+    vec3 h = max(0.5 - vec3(dot(a,a), dot(b,b), dot(c,c)), 0.0);
+
+    vec3 n = h * h * h * h * vec3(
+        dot(a, hash(i + 0.0)),
+        dot(b, hash(i + o)),
+        dot(c, hash(i + 1.0))
+    );
+
+    return dot(n, vec3(70.0));
+}
+
+
+float fbm(vec2 p){
+  float a = .5;
+  float n = 0.;
+
+  for(float i=0.;i<4.;i++){
+    n += a * noise(p);
+    p *= 2.;
+    a *= .5;
+  }
+  return n;
+}
+
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = I/R.y;
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  uv+=noise(uv+T*0.1)*0.1;
+
+  // 使用噪音和缩放制作锯齿形状
+  float d = fbm(uv*vec2(8.,1.1)+vec2(0.,T));
+  d = uv.y+d*0.8;
+
+  vec3 c = vec3(3,2,1)+d*d;
+
+  c = tanh(c*0.4);
+
+  O.rgb = d*c;
+
+}
