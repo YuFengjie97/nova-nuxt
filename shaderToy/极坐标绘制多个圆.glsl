d3d7dfc07b2e69583396dc81794e3a5b6f084dd4
yufengjie <2457522528@qq.com>
2025-06-05 17:25:38 +0800

    1

--- shaderToy/极坐标绘制多个圆.glsl
+++ shaderToy/极坐标绘制多个圆.glsl
@@ -6,17 +6,20 @@ void mainImage(out vec4 O, in vec2 I){
   vec2 uv = (I*2.-R)/R.y;
   O.rgb *= 0.;
   O.a = 1.;
-  
+
+  uv*=4.;
+
   vec2 p = uv;
-  p *= 2.;
 
   p = vec2(atan(p.y,p.x)/(Pi*0.25), length(p));
 
   vec2 grid = min(vec2(1), .5*fwidth(p)/abs(p-round(p)));
   O.rgb += max(grid.x, grid.y)*0.5;
 
-
-  float d = length(p);
-
+  vec2 pos = floor(p);
+  pos = vec2(cos(pos.x)*pos.y, sin(pos.x)*pos.y);
+  float d = length(uv-pos)-0.1;
+  d = smoothstep(0.2,0.19,d);
   O.rgb += d;
+
 }
