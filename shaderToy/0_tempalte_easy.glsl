7cef8e98f5da3942e68d00c2c5a7a1397c63b3c4
yufengjie <2457522528@qq.com>
2025-07-15 13:03:58 +0800

    template

--- shaderToy/0_tempalte_easy.glsl
+++ shaderToy/0_tempalte_easy.glsl
@@ -3,10 +3,9 @@
 #define S smoothstep
 
 
-vec4 map(vec3 p) {
-  vec3 col = vec3(0);
-  float d = 0.;
-  return vec4(col, d);
+float map(vec3 p) {
+  float d = length(p)- 4.;
+  return d;
 }
 
 
@@ -16,10 +15,10 @@ vec3 calcNormal( in vec3 pos )
     vec2 e = vec2(1.0,-1.0);
     const float eps = 0.0005;
     return normalize( 
-            e.xyy*map( pos + e.xyy*eps ).w + 
-					  e.yyx*map( pos + e.yyx*eps ).w + 
-					  e.yxy*map( pos + e.yxy*eps ).w + 
-					  e.xxx*map( pos + e.xxx*eps ).w );
+            e.xyy*map( pos + e.xyy*eps ) + 
+					  e.yyx*map( pos + e.yyx*eps ) + 
+					  e.yxy*map( pos + e.yxy*eps ) + 
+					  e.xxx*map( pos + e.xxx*eps ) );
 }
 
 
@@ -40,25 +39,23 @@ void mainImage(out vec4 O, in vec2 I){
   vec3 ro = vec3(0.,0.,-10.);
   vec3 rd = normalize(vec3(uv, 1.));
 
-  float z;
-  float d = 1e10;
+  float z = 0.;
+  float zMax = 100.;
 
   for(float i =0.;i<100.;i++){
     vec3 p = ro + rd * z;
-    p.xz *= rotate(T);
-    p.xy *= rotate(T);
 
-    vec3 q = abs(p)-vec3(1.,2.,3.);
-    float d1 = max(q.x,max(q.y,q.z));
-    d = min(d, d1);
-    d = max(0.01,d);
-
-    O.rgb += (1.1+sin(vec3(3,2,1)+(p.z)*0.2))/d;
+    float d = map(p);
+    if(d<1e-3) break;
     z += d;
-
-    if(z>100. || d<1e-3) break;
+    if(z>zMax) break;
   }
 
-  O.rgb = tanh(O.rgb / 1e4);
-
+  if(z<zMax) {
+    vec3 col = vec3(1,0,0);
+    vec3 p = ro + rd * z;
+    vec3 nor = calcNormal(p);
+    float diff = clamp(dot(nor, vec3(0,0,-4)),0.,1.);
+    O = vec4(col*diff, 1);
+  }
 }
