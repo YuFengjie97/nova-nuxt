31eeea535e4418b01de9a35702d1130022533b1f
yufengjie <2457522528@qq.com>
2025-07-27 09:07:59 +0800

    1

---
+++ shaderToy/caveFrom2Dsdf.glsl
@@ -0,0 +1,178 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define S smoothstep
+
+
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+float sdBox( vec3 p, vec3 b )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float sdBox( in vec2 p, in vec2 b )
+{
+    vec2 d = abs(p)-b;
+    return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+}
+float sdEquilateralTriangle( in vec2 p, in float r )
+{
+    const float k = sqrt(3.0);
+    p.x = abs(p.x) - r;
+    p.y = p.y + r/k;
+    if( p.x+k*p.y>0.0 ) p = vec2(p.x-k*p.y,-k*p.x-p.y)/2.0;
+    p.x -= clamp( p.x, -2.0*r, 0.0 );
+    return -length(p)*sign(p.y);
+}
+
+
+vec4 map(vec3 p) {
+  // p.xz *= rotate(T*.5);
+  p.z += T*5.;
+  float d1 = sdEquilateralTriangle(p.xy, (4.));
+  float d2 = sdBox(p.xy, vec2(4.));
+  float dd = mix(d1, d2, sin(T*.5)*.5+.5);
+
+  float d = length(vec2(dd, dd))-2.;
+
+  vec3 col = sin(vec3(3,2,1)+(p.z)*.4)*.5+.5;
+  // vec3 col = vec3(.5);
+  return vec4(col, d);
+}
+
+// https://www.shadertoy.com/view/lsKcDD
+mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
+{
+	vec3 cw = normalize(ta-ro);            // 相机前
+	vec3 cp = vec3(sin(cr), cos(cr),0.0);  // 滚角
+	vec3 cu = normalize( cross(cw,cp) );   // 相机右
+	vec3 cv = normalize( cross(cu,cw) );   // 相机上
+  return mat3( cu, cv, cw );
+}
+
+// https://www.shadertoy.com/view/lsKcDD
+float calcAO( in vec3 pos, in vec3 nor )
+{
+	float occ = 0.0;
+    float sca = 1.0;
+    for( int i=0; i<5; i++ )
+    {
+        float h = 0.001 + 0.15*float(i)/4.0;
+        float d = map( pos + h*nor ).w;
+        occ += (h-d)*sca;
+        sca *= 0.95;
+    }
+    return clamp( 1.0 - 1.5*occ, 0.0, 1.0 );    
+}
+
+
+
+// https://iquilezles.org/articles/normalsSDF/
+vec3 calcNormal( in vec3 pos )
+{
+    vec2 e = vec2(1.0,-1.0);
+    const float eps = 0.0005;
+    return normalize( 
+            e.xyy*map( pos + e.xyy*eps ).w + 
+					  e.yyx*map( pos + e.yyx*eps ).w + 
+					  e.yxy*map( pos + e.yxy*eps ).w + 
+					  e.xxx*map( pos + e.xxx*eps ).w );
+}
+
+vec3 calcNormal2(vec3 pos){
+  vec2 e = vec2(0.0005,0);
+  return normalize(
+    vec3(
+      map(pos+e.xyy).w,
+      map(pos+e.yxy).w,
+      map(pos+e.yyx).w
+    )-map(pos).w
+  );
+}
+
+// https://www.shadertoy.com/view/MtsGWH
+vec4 boxmap( in sampler2D s, in vec3 p, in vec3 n, in float k )
+{
+    // project+fetch
+	vec4 x = texture( s, p.yz );
+	vec4 y = texture( s, p.zx );
+	vec4 z = texture( s, p.xy );
+    
+    // and blend
+  vec3 m = pow( abs(n), vec3(k) );
+	return (x*m.x + y*m.y + z*m.z) / (m.x + m.y + m.z);
+}
+
+float rayMarch(vec3 ro, vec3 rd, float zMin, float zMax){
+  float z = zMin;
+  for(float i=0.;i<100.;i++){
+    vec3 p = ro + rd * z;
+    float d = map(p).w*.7;
+    if(d<1e-3 || z>zMax) break;
+    z += d;
+  }
+
+  return z;
+}
+
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  vec3 ro = vec3(0.,0.,-10.);
+  // ro.xz = vec2(cos(T), sin(T))*10.;
+
+  vec3 rd = normalize(vec3(uv, 1.));
+  // vec3 rd = normalize(setCamera(ro, vec3(0), 0.)*vec3(uv, 1.));
+
+  float zMax = 50.;
+
+  float z = rayMarch(ro, rd, 0.1, zMax);
+
+  vec3 col = vec3(0);
+  if(z<zMax) {
+    vec3 p = ro + rd * z;
+    vec3 nor = calcNormal(p);
+    // vec3 objCol = boxmap(iChannel0, p*.1, nor, 7.).rgb;
+    vec3 objCol = map(p).rgb;
+    // vec3 objCol = vec3(1,0,0);
+
+    float amb = .5;
+    col += objCol * amb;
+
+    vec3 l_dir = normalize(vec3(cos(T*.1)*2.,sin(T*.1)*2.,-4)-p);
+    float diff = max(0., dot(l_dir, nor));
+    col += objCol * diff;
+
+    // float spe = pow(max(0., dot(reflect(-l_dir, nor), -rd)), 5.);
+    float spe = pow(max(0., dot(normalize(l_dir-rd), nor)), 60.);
+    vec3 speCol = vec3(1);
+    col += spe * speCol;
+
+    // float fre = pow(max(1.-dot(nor, -rd),0.),3.);
+    // vec3 freCol = vec3(1,1,0);
+    // col += objCol * fre * freCol;
+
+    col *= calcAO(p, nor);
+  }
+
+  col *= exp(-1e-5*z*z*z);
+  // col = pow(col, vec3(.5));
+  O.rgb = col;
+
+}
