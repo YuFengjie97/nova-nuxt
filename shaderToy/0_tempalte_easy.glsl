f4b0a0eecf7b969b9cae769b59b42c094d4eb41a
yufengjie <2457522528@qq.com>
2025-08-06 10:21:40 +0800

    模板

--- shaderToy/0_tempalte_easy.glsl
+++ shaderToy/0_tempalte_easy.glsl
@@ -14,9 +14,22 @@ mat2 rotate(float a){
   return mat2(c,-s,s,c);
 }
 
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
   vec2 uv = (I*2.-R)/R.y;
+  vec2 m = iMouse.xy/R * 5.;
 
   O.rgb *= 0.;
   O.a = 1.;
@@ -31,8 +44,17 @@ void mainImage(out vec4 O, in vec2 I){
   vec3 col = vec3(0);
   for(float i=0.;i<100.;i++){
     vec3 p = ro + rd * z;
+
+    if(iMouse.z>0.){
+      p.xz *= rotate(m.x);
+      p.yz *= rotate(m.y);
+    }
+
     float d = length(p) - 2.;
-    d = abs(d*.3) + .1;
+    d += fbm(p*2.);
+
+    d = abs(d) + .1;
+
     
     col += (1.1+sin(vec3(3,2,1)+p.x))/d;
     
@@ -40,7 +62,7 @@ void mainImage(out vec4 O, in vec2 I){
     z += d;
   }
 
-  col = tanh(col / 5e2);
+  col = tanh(col / 1e2);
 
   O.rgb = col;
 }
