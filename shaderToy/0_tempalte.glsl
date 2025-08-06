511ee2c49a178ac4cc0e3428f7122e0277564896
yufengjie <2457522528@qq.com>
2025-08-06 10:16:57 +0800

    模板调整

--- shaderToy/0_tempalte.glsl
+++ shaderToy/0_tempalte.glsl
@@ -1,5 +1,13 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
 #define T iTime
 #define PI 3.141596
+#define S smoothstep
+const float EPSILON = 1e-6;
+
 
 
 mat2 rotate(float a){
@@ -8,21 +16,51 @@ mat2 rotate(float a){
   return mat2(c,-s,s,c);
 }
 
-
-float map(vec3 p){
-  float d;
-  float freq = .5;
-
-  // xor https://mini.gmshaders.com/p/turbulence
-  for(float i=1.;i<6.;i*=1.5){
-    p += sin(p.zxy * freq  + T*0.05) / freq;
-    freq *= 2.;
-  }
-  d =  abs(length(p.xy)-9.) / 30.;
-
-  return d;
-
+float sdBox( vec3 p, vec3 b )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
 }
+
+struct Surface{
+  float d;
+  vec3 col;
+};
+
+Surface map(vec3 p) {
+  p.xz *= rotate(T*.5);
+  float d = length(p)- 4.;
+  vec3 col = sin(vec3(3,2,1)+(p.x+p.y+p.z)*.4)*.5+.5;
+  // vec3 col = vec3(.5);
+  return Surface(d, col);
+}
+
+// https://www.shadertoy.com/view/lsKcDD
+mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
+{
+	vec3 cw = normalize(ta-ro);            // 相机前
+	vec3 cp = vec3(sin(cr), cos(cr),0.0);  // 滚角
+	vec3 cu = normalize( cross(cw,cp) );   // 相机右
+	vec3 cv = normalize( cross(cu,cw) );   // 相机上
+  return mat3( cu, cv, cw );
+}
+
+// https://www.shadertoy.com/view/lsKcDD
+float calcAO( in vec3 pos, in vec3 nor )
+{
+	float occ = 0.0;
+    float sca = 1.0;
+    for( int i=0; i<5; i++ )
+    {
+        float h = 0.001 + 0.15*float(i)/4.0;
+        float d = map( pos + h*nor ).d;
+        occ += (h-d)*sca;
+        sca *= 0.95;
+    }
+    return clamp( 1.0 - 1.5*occ, 0.0, 1.0 );    
+}
+
+
 
 // https://iquilezles.org/articles/normalsSDF/
 vec3 calcNormal( in vec3 pos )
@@ -30,40 +68,56 @@ vec3 calcNormal( in vec3 pos )
     vec2 e = vec2(1.0,-1.0);
     const float eps = 0.0005;
     return normalize( 
-            e.xyy*map( pos + e.xyy*eps ) + 
-					  e.yyx*map( pos + e.yyx*eps ) + 
-					  e.yxy*map( pos + e.yxy*eps ) + 
-					  e.xxx*map( pos + e.xxx*eps ) );
+            e.xyy*map( pos + e.xyy*eps ).d + 
+					  e.yyx*map( pos + e.yyx*eps ).d + 
+					  e.yxy*map( pos + e.yxy*eps ).d + 
+					  e.xxx*map( pos + e.xxx*eps ).d );
 }
 
+vec3 calcNormal2(vec3 pos){
+  vec2 e = vec2(0.0005,0);
+  return normalize(
+    vec3(
+      map(pos+e.xyy).d,
+      map(pos+e.yxy).d,
+      map(pos+e.yyx).d
+    )-map(pos).d
+  );
+}
 
-// https://iquilezles.org/articles/normalsSDF/
-vec3 calcNormal4( in vec3 p ) // for function f(p)
+// https://www.shadertoy.com/view/MtsGWH
+vec4 boxmap( in sampler2D s, in vec3 p, in vec3 n, in float k )
 {
-    const float h = 0.01;      // replace by an appropriate value
-    #define ZERO (min(iFrame,0)) // non-constant zero
-    vec3 n = vec3(0.0);
-    for( int i=ZERO; i<4; i++ )
-    {
-        vec3 e = 0.5773*(2.0*vec3((((i+3)>>1)&1),((i>>1)&1),(i&1))-1.0);
-        n += e*map(p+e*h);
-    }
-    return normalize(n);
+    // project+fetch
+	vec4 x = texture( s, p.yz );
+	vec4 y = texture( s, p.zx );
+	vec4 z = texture( s, p.xy );
+    
+    // and blend
+  vec3 m = pow( abs(n), vec3(k) );
+	return (x*m.x + y*m.y + z*m.z) / (m.x + m.y + m.z);
 }
 
-vec3 rayMarch(vec3 ro, vec3 rd){
-  vec3 p;
+struct RM{
   float z;
-  for(float i=0.;i<100.;i++){
-    p = ro + rd * z;
-    p.z += T;
-    
-    float d = map(p);
-    z += d;
+  bool hit;
+};
 
-    if(z>50. || d<1e-2) break;
+RM rayMarch(vec3 ro, vec3 rd, float zMin, float zMax){
+  float z = zMin;
+  bool hit = false;
+  for(float i=0.;i<100.;i++){
+    vec3 p = ro + rd * z;
+    float d = map(p).d;
+    if(d<EPSILON ){
+      hit = true;
+      break;
+    }
+    if(z>zMax) break;
+    z += d;
   }
-  return p;
+
+  return RM(z, hit);
 }
 
 
@@ -71,22 +125,43 @@ void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
   vec2 uv = (I*2.-R)/R.y;
 
-
   O.rgb *= 0.;
   O.a = 1.;
 
   vec3 ro = vec3(0.,0.,-10.);
+  // ro.xz = vec2(cos(T), sin(T))*10.;
+
   vec3 rd = normalize(vec3(uv, 1.));
-  vec3 p = rayMarch(ro, rd);
-  float d = map(p);
+  // vec3 rd = setCamera(ro, vec3(0), 0.)*normalize(vec3(uv, 1.));
 
+  float zMax = 50.;
 
-  vec3 light = normalize(vec3(1,1,p.z)-p);
-  vec3 nor = calcNormal4(p);
-  float dif = clamp(dot(light, nor), 0., 1.);
-  // float dif = dot(light, nor);
-  
-  O.rgb = sin(vec3(3,2,1)+d*30.+p.z*0.1);
-  O.rgb += dif*dif*vec3(1,1,0)*0.8;
-  //O.rgb = tanh(O.rgb * 0.9);
+  RM rm = rayMarch(ro, rd, 0.1, zMax);
+  bool hit = rm.hit;
+  float z = rm.z;
+
+  vec3 col = vec3(0);
+  if(hit) {
+    vec3 p = ro + rd * z;
+    vec3 nor = calcNormal(p);
+    // vec3 objCol = boxmap(iChannel0, p*.1, nor, 7.).rgb;
+    vec3 objCol = map(p).col;
+    // vec3 objCol = vec3(1,0,0);
+
+    vec3 l_dir = normalize(vec3(4,4,-4)-p);
+    float diff = max(0., dot(l_dir, nor));
+
+    // float spe = pow(max(0., dot(reflect(-l_dir, nor), -rd)), 5.);
+    float spe = pow(max(0., dot(normalize(l_dir-rd), nor)), 30.);
+
+    float fre = pow(max(1.-dot(nor, -rd),0.),3.);
+    col = objCol * diff + spe + fre*.1;
+
+    // col *= calcAO(p, nor);
+  }
+
+  // col *= exp(-1e-4*z*z*z);
+  col = pow(col, vec3(.4545));
+  O.rgb = col;
+
 }
--- shaderToy/0_tempalte_easy.glsl
+++ shaderToy/0_tempalte_easy.glsl
@@ -8,118 +8,11 @@
 #define S smoothstep
 const float EPSILON = 1e-6;
 
-
-
 mat2 rotate(float a){
   float s = sin(a);
   float c = cos(a);
   return mat2(c,-s,s,c);
 }
-
-float sdBox( vec3 p, vec3 b )
-{
-  vec3 q = abs(p) - b;
-  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
-}
-
-struct Surface{
-  float d;
-  vec3 col;
-};
-
-Surface map(vec3 p) {
-  p.xz *= rotate(T*.5);
-  float d = length(p)- 4.;
-  vec3 col = sin(vec3(3,2,1)+(p.x+p.y+p.z)*.4)*.5+.5;
-  // vec3 col = vec3(.5);
-  return Surface(d, col);
-}
-
-// https://www.shadertoy.com/view/lsKcDD
-mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
-{
-	vec3 cw = normalize(ta-ro);            // 相机前
-	vec3 cp = vec3(sin(cr), cos(cr),0.0);  // 滚角
-	vec3 cu = normalize( cross(cw,cp) );   // 相机右
-	vec3 cv = normalize( cross(cu,cw) );   // 相机上
-  return mat3( cu, cv, cw );
-}
-
-// https://www.shadertoy.com/view/lsKcDD
-float calcAO( in vec3 pos, in vec3 nor )
-{
-	float occ = 0.0;
-    float sca = 1.0;
-    for( int i=0; i<5; i++ )
-    {
-        float h = 0.001 + 0.15*float(i)/4.0;
-        float d = map( pos + h*nor ).d;
-        occ += (h-d)*sca;
-        sca *= 0.95;
-    }
-    return clamp( 1.0 - 1.5*occ, 0.0, 1.0 );    
-}
-
-
-
-// https://iquilezles.org/articles/normalsSDF/
-vec3 calcNormal( in vec3 pos )
-{
-    vec2 e = vec2(1.0,-1.0);
-    const float eps = 0.0005;
-    return normalize( 
-            e.xyy*map( pos + e.xyy*eps ).d + 
-					  e.yyx*map( pos + e.yyx*eps ).d + 
-					  e.yxy*map( pos + e.yxy*eps ).d + 
-					  e.xxx*map( pos + e.xxx*eps ).d );
-}
-
-vec3 calcNormal2(vec3 pos){
-  vec2 e = vec2(0.0005,0);
-  return normalize(
-    vec3(
-      map(pos+e.xyy).d,
-      map(pos+e.yxy).d,
-      map(pos+e.yyx).d
-    )-map(pos).d
-  );
-}
-
-// https://www.shadertoy.com/view/MtsGWH
-vec4 boxmap( in sampler2D s, in vec3 p, in vec3 n, in float k )
-{
-    // project+fetch
-	vec4 x = texture( s, p.yz );
-	vec4 y = texture( s, p.zx );
-	vec4 z = texture( s, p.xy );
-    
-    // and blend
-  vec3 m = pow( abs(n), vec3(k) );
-	return (x*m.x + y*m.y + z*m.z) / (m.x + m.y + m.z);
-}
-
-struct RM{
-  float z;
-  bool hit;
-};
-
-RM rayMarch(vec3 ro, vec3 rd, float zMin, float zMax){
-  float z = zMin;
-  bool hit = false;
-  for(float i=0.;i<100.;i++){
-    vec3 p = ro + rd * z;
-    float d = map(p).d;
-    if(d<EPSILON ){
-      hit = true;
-      break;
-    }
-    if(z>zMax) break;
-    z += d;
-  }
-
-  return RM(z, hit);
-}
-
 
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
@@ -129,39 +22,25 @@ void mainImage(out vec4 O, in vec2 I){
   O.a = 1.;
 
   vec3 ro = vec3(0.,0.,-10.);
-  // ro.xz = vec2(cos(T), sin(T))*10.;
 
   vec3 rd = normalize(vec3(uv, 1.));
-  // vec3 rd = setCamera(ro, vec3(0), 0.)*normalize(vec3(uv, 1.));
 
   float zMax = 50.;
-
-  RM rm = rayMarch(ro, rd, 0.1, zMax);
-  bool hit = rm.hit;
-  float z = rm.z;
+  float z = .1;
 
   vec3 col = vec3(0);
-  if(hit) {
+  for(float i=0.;i<100.;i++){
     vec3 p = ro + rd * z;
-    vec3 nor = calcNormal(p);
-    // vec3 objCol = boxmap(iChannel0, p*.1, nor, 7.).rgb;
-    vec3 objCol = map(p).col;
-    // vec3 objCol = vec3(1,0,0);
-
-    vec3 l_dir = normalize(vec3(4,4,-4)-p);
-    float diff = max(0., dot(l_dir, nor));
-
-    // float spe = pow(max(0., dot(reflect(-l_dir, nor), -rd)), 5.);
-    float spe = pow(max(0., dot(normalize(l_dir-rd), nor)), 30.);
-
-    float fre = pow(max(1.-dot(nor, -rd),0.),3.);
-    col = objCol * diff + spe + fre*.1;
-
-    // col *= calcAO(p, nor);
+    float d = length(p) - 2.;
+    d = abs(d*.3) + .1;
+    
+    col += (1.1+sin(vec3(3,2,1)+p.x))/d;
+    
+    if(d<EPSILON || z>zMax) break;
+    z += d;
   }
 
-  // col *= exp(-1e-4*z*z*z);
-  col = pow(col, vec3(.4545));
-  O.rgb = col;
+  col = tanh(col / 5e2);
 
+  O.rgb = col;
 }
