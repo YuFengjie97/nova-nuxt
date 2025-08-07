417995a76fdc85729e498d9de97d8b045fb2039f
yufengjie <2457522528@qq.com>
2025-08-07 16:03:01 +0800

    template

--- shaderToy/0_tempalte.glsl
+++ shaderToy/0_tempalte.glsl
@@ -26,16 +26,22 @@ Obj obj_union(Obj o1, Obj o2){
   return o2;
 }
 
-float sdBox( vec3 p, vec3 b )
+
+float sdRoundBox( vec3 p, vec3 b, float r )
 {
-  vec3 q = abs(p) - b;
-  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+  vec3 q = abs(p) - b + r;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
 }
 
 
 Obj map(vec3 p) {
-  p.xz *= rotate(T*.5);
-  float d = length(p)- 4.;
+  vec2 m = (iMouse.xy*2.-iResolution.xy)/iResolution.xy*6.;
+  if(iMouse.z>0.){
+    p.xz*=rotate(m.x);
+    p.yz*=rotate(m.y);
+  }
+  float d1 = sdRoundBox(p, vec3(4, 1., 1.), .1);
+  float d = d1;
   return Obj(d, 1.);
 }
 
@@ -128,6 +134,7 @@ RM rayMarch(vec3 ro, vec3 rd, float zMin, float zMax){
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
   vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R*6.;
 
   O.rgb *= 0.;
   O.a = 1.;
@@ -147,10 +154,11 @@ void mainImage(out vec4 O, in vec2 I){
   vec3 col = vec3(0);
   if(hit) {
     vec3 p = ro + rd * z;
+
     vec3 nor = calcNormal(p);
     // vec3 objCol = boxmap(iChannel0, p*.1, nor, 7.).rgb;
+    
     Obj obj = map(p);
-    // vec3 objCol = vec3(1,0,0);
     if(obj.id == 1.){
       col = vec3(1,0,0);
     }
