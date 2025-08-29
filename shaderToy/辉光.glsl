1e9b59c3dce7884e00cbf950e1a885f878d45dce
yufengjie <2457522528@qq.com>
2025-08-29 12:47:37 +0800

    raymarch命中测试

--- shaderToy/辉光.glsl
+++ shaderToy/辉光.glsl
@@ -16,6 +16,31 @@ mat2 rotate(float a){
   return mat2(c,-s,s,c);
 }
 
+float map(vec3 p){
+    vec3 q = p;
+
+    q += cos(q.zyy*1.);
+    q += cos(q.zxx*.5)*1.5;
+    q += cos(q.xxy*.25)*2.;
+
+    q.xz *= rotate(q.y*3.+T);
+
+    float D = length(q.xy);
+    float s = 2.;
+    float id = round(D / s);
+    id = clamp(id, 0., 3.);
+    D -= id * s;
+    float Rc = .5;
+    float rc = .3;
+
+
+    float d = length(vec2(D-Rc, q.z))-rc;
+
+    // d = max(d, 0.);
+    d = abs(d) + .1;
+    return d;
+}
+
 
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
@@ -41,31 +66,24 @@ void mainImage(out vec4 O, in vec2 I){
     //   p.yz *= rotate(m.y);
     // }
 
-    vec3 q = p;
-
-    q += cos(q.zyy*1.);
-    q += cos(q.zxx*.5)*1.5;
-    q += cos(q.xxy*.25)*2.;
-
-    q.xz *= rotate(q.y*3.+T);
-
-    float D = length(q.xy);
-    float s = 2.;
-    float id = round(D / s);
-    id = clamp(id, 0., 3.);
-    D -= id * s;
-    float Rc = .5;
-    float rc = .3;
-
-
-    float d = length(vec2(D-Rc, q.z))-rc;
-
-    // d = max(d, 0.);
-    d = abs(d) + .1;
+    float d = map(p);
 
     col += (1.+sin(vec3(3,2,1)+p.x*.8+p.z*.2))/d;
     
-    if(d<EPSILON || z>zMax) break;
+    if(z>zMax) break;
+
+    // z += d;
+    // if(d<.1){
+    //   float t1 = z - d;
+    //   float t2 = z;
+    //   for (int i = 0; i < 5; i++) {
+    //     float mid = 0.5 * (t1 + t2);
+    //     float dm = map(ro + rd * mid);
+    //     if (dm < 0.0) t2 = mid; else t1 = mid;
+    //   }
+    //   z = 0.5 * (t1 + t2); // 更精确的命中点
+    //   break;
+    // }
     z += d*.02;
   }
 
