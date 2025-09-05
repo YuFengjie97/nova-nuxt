2731c890b86aa30aae04cfe0d53c24defdb99025
yufengjie <2457522528@qq.com>
2025-09-05 12:48:22 +0800

    ss

---
+++ shaderToy/线段或矩形的简易距离场.glsl
@@ -0,0 +1,46 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+
+const float EPSILON = 1e-6;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  float w = 0.4 * s1(T);
+  float h = .4;
+  float r = 0.4 * s1(T);
+  float d = length(max(abs(uv)-vec2(w,h),0.))-r;
+  d = S(0.01,0.,d);
+  O.rgb += d;
+}
