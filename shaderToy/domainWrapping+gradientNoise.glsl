f7feaaeb652e302881d0a653431ec5aa9a44f1ba
yufengjie <2457522528@qq.com>
2025-09-07 14:34:28 +0800

    1

---
+++ shaderToy/domainWrapping+gradientNoise.glsl
@@ -0,0 +1,100 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-6;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+float hash(vec2 p){
+  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
+}
+
+vec2 randomGradient(vec2 p){
+  float a = hash(p)*TAU;
+  a += T;
+  return vec2(cos(a), sin(a));
+}
+
+float noise(vec2 p){
+  vec2 i = floor(p);
+  vec2 f = fract(p);
+
+  vec2 g00 = randomGradient(i+vec2(0,0));
+  vec2 g10 = randomGradient(i+vec2(1,0));
+  vec2 g01 = randomGradient(i+vec2(0,1));
+  vec2 g11 = randomGradient(i+vec2(1,1));
+
+  float v00 = dot(g00, f-vec2(0,0));
+  float v10 = dot(g10, f-vec2(1,0));
+  float v01 = dot(g01, f-vec2(0,1));
+  float v11 = dot(g11, f-vec2(1,1));
+
+  vec2 u = smoothstep(0.,1.,f);
+
+  return mix(mix(v00,v10,u.x), mix(v01,v11,u.x), u.y);
+}
+
+float fbm(vec2 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<6.;i++){
+    n += noise(fre*p)*amp;
+    p *= rotate(2.+T*.01);
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+float pattern( in vec2 p )
+{
+    vec2 q = vec2( fbm( p + vec2(0.0,0.0) ),
+                   fbm( p + vec2(5.2,1.3) ) );
+
+    return fbm( p + 4.0*q );
+}
+float pattern2( in vec2 p )
+{
+    vec2 q = vec2( fbm( p + vec2(0.0,0.0) ),
+                   fbm( p + vec2(5.2,1.3) ) );
+
+    vec2 r = vec2( fbm( p + 4.0*q + vec2(1.7,9.2) ),
+                   fbm( p + 4.0*q + vec2(8.3,2.8) ) );
+
+    return fbm( p + 4.0*r );
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  float n = pattern(abs(uv))+.5;
+  vec3 c = s1(vec3(4,2,1)+n*10.+T) * 1.8;
+  // vec3 c = palette(n*2.+T)*2.;
+
+  O.rgb = c * n;
+}
