e77817c0099e6ec8fb27c78ff358169eab367728
yufengjie <2457522528@qq.com>
2025-09-28 13:31:40 +0800

    1

---
+++ shaderToy/不同尺寸的域重复.glsl
@@ -0,0 +1,147 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-6;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+float hash(vec2 p){
+  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
+}
+
+vec2 randomGradient(vec2 p){
+  float a = hash(p)*TAU;
+  a += T;
+  return vec2(cos(a), sin(a));
+}
+
+float noise(vec2 p){
+  vec2 i = floor(p);
+  vec2 f = fract(p);
+
+  vec2 g00 = randomGradient(i+vec2(0,0));
+  vec2 g10 = randomGradient(i+vec2(1,0));
+  vec2 g01 = randomGradient(i+vec2(0,1));
+  vec2 g11 = randomGradient(i+vec2(1,1));
+
+  float v00 = dot(g00, f-vec2(0,0));
+  float v10 = dot(g10, f-vec2(1,0));
+  float v01 = dot(g01, f-vec2(0,1));
+  float v11 = dot(g11, f-vec2(1,1));
+
+  vec2 u = smoothstep(0.,1.,f);
+
+  return mix(mix(v00,v10,u.x), mix(v01,v11,u.x), u.y);
+}
+
+float fbm(vec2 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<6.;i++){
+    n += noise(fre*p)*amp;
+    p *= rotate(2.+T*.01);
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+float sdBox( vec3 p, vec3 b )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  vec3 ro = vec3(0.,1.,-4.);
+
+  vec3 rd = normalize(vec3(uv, 1.));
+
+  float zMax = 50.;
+  float z = .1;
+
+  vec3 col = vec3(0);
+  for(float i=0.;i<100.;i++){
+    vec3 p = ro + rd * z;
+
+    if(iMouse.z>0.){
+      p.xz *= rotate(m.x);
+      p.yz *= rotate(m.y);
+    }
+
+    // base repetition
+    vec2 idd = floor(p.xy/2.);
+    idd = clamp(idd,-1.,1.);
+    p.xy -= idd*2.;
+    p.xy -= 1.;
+
+
+    vec3 q1 = p;
+    vec3 q2 = p+vec3(.5,0,0);
+
+
+    // big size repetition
+    float id = floor(q1.x/2.);
+    vec3 cen = vec3(id+.5, 0, 0)*2.;
+    float d = length(q1-cen) - .5;
+    d = max(0., d);    
+    col += s1(vec3(3,2,1)+id+idd.y)/d;
+
+    // small size repetition
+    {
+      float id = floor(q2.x);
+      vec3 cen = vec3(id+.5, 0, 0);
+      float d1 = sdBox(q2-cen, vec3(.2));
+      d1 = max(0., d1);    
+      d = min(d, d1);
+      col += s1(vec3(3,2,1)+id)/d1;
+    }
+
+    if(d<EPSILON || z>zMax) break;
+    z += d;
+  }
+
+  col = tanh(col / 1e2);
+
+  O.rgb = col;
+}
