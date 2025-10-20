c87239641d9ce3824e56472b3bbcbc9a8515e387
yufengjie <2457522528@qq.com>
2025-10-20 10:10:54 +0800

    1

---
+++ shaderToy/boxIntersection.glsl
@@ -0,0 +1,143 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-3;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+float hash(vec2 p){
+  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
+}
+
+vec2 randomGradient(vec2 p){
+  float a = hash(p)*TAU;
+  a += T;
+  return vec2(cos(a), sin(a));
+}
+
+float noise(vec2 p){
+  vec2 i = floor(p);
+  vec2 f = fract(p);
+
+  vec2 g00 = randomGradient(i+vec2(0,0));
+  vec2 g10 = randomGradient(i+vec2(1,0));
+  vec2 g01 = randomGradient(i+vec2(0,1));
+  vec2 g11 = randomGradient(i+vec2(1,1));
+
+  float v00 = dot(g00, f-vec2(0,0));
+  float v10 = dot(g10, f-vec2(1,0));
+  float v01 = dot(g01, f-vec2(0,1));
+  float v11 = dot(g11, f-vec2(1,1));
+
+  vec2 u = smoothstep(0.,1.,f);
+
+  return mix(mix(v00,v10,u.x), mix(v01,v11,u.x), u.y);
+}
+
+float fbm(vec2 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<6.;i++){
+    n += noise(fre*p)*amp;
+    p *= rotate(2.+T*.01);
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+vec2 boxIntersection( in vec3 ro, in vec3 rd, vec3 boxSize ) 
+{
+    vec3 m = 1.0/rd; // can precompute if traversing a set of aligned boxes
+    vec3 n = m*ro;   // can precompute if traversing a set of aligned boxes
+    vec3 k = abs(m)*boxSize;
+    vec3 t1 = -n - k;
+    vec3 t2 = -n + k;
+    float tN = max( max( t1.x, t1.y ), t1.z );
+    float tF = min( min( t2.x, t2.y ), t2.z );
+    if( tN>tF || tF<0.0) return vec2(-1.0); // no intersection
+    // outNormal = (tN>0.0) ? step(vec3(tN),t1)) : // ro ouside the box
+    //                        step(t2,vec3(tF)));  // ro inside the box
+    // outNormal *= -sign(rd);
+    return vec2( tN, tF );
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+  vec3 col = vec3(0);
+
+
+  vec3 ro = vec3(0.,0.,-10.);
+  vec3 rd = normalize(vec3(uv, 1.));
+
+  vec3 pos = vec3(cos(T)*4.,sin(T)*4.,5);
+  float rad = 2.;
+  vec3 boxSize = vec3(rad);
+
+  vec2 bound = boxIntersection(ro-pos, rd, boxSize);
+
+  if(bound.x<0.){
+    //
+  }else{
+    ro += rd * bound.x;
+
+    float z = 0.;
+    for(float i=0.;i<100.;i++){
+      vec3 p = ro + rd * z;
+
+      float d = length(p-pos) - rad;
+      // d += fbm(p*2.);
+
+      // d = abs(d) + .001;
+      // d = max(0., d);
+
+      if(d<EPSILON){
+        col = (1.1+sin(vec3(3,2,1)+10.));
+      }
+      if(z>bound.y) break;
+      z += d;
+    }
+  }
+
+  // col = tanh(col / 1e2);
+
+  O.rgb = col;
+}
