2d1d41db2d6a54eeef0e1c69868a13ef6438bf18
yufengjie <2457522528@qq.com>
2025-10-22 20:07:06 +0800

    tonemap

---
+++ shaderToy/tonemap.glsl
@@ -0,0 +1,132 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-6;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+float hash(vec2 p){
+  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
+}
+
+vec2 randomGradient(vec2 p){
+  float a = hash(p)*TAU;
+  a += T;
+  return vec2(cos(a), sin(a));
+}
+
+float noise(vec2 p){
+  vec2 i = floor(p);
+  vec2 f = fract(p);
+
+  vec2 g00 = randomGradient(i+vec2(0,0));
+  vec2 g10 = randomGradient(i+vec2(1,0));
+  vec2 g01 = randomGradient(i+vec2(0,1));
+  vec2 g11 = randomGradient(i+vec2(1,1));
+
+  float v00 = dot(g00, f-vec2(0,0));
+  float v10 = dot(g10, f-vec2(1,0));
+  float v01 = dot(g01, f-vec2(0,1));
+  float v11 = dot(g11, f-vec2(1,1));
+
+  vec2 u = smoothstep(0.,1.,f);
+
+  return mix(mix(v00,v10,u.x), mix(v01,v11,u.x), u.y);
+}
+
+float fbm(vec2 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<6.;i++){
+    n += noise(fre*p)*amp;
+    p *= rotate(2.+T*.01);
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+//https://mini.gmshaders.com/p/tonemaps
+vec3 Tonemap_ACES(vec3 x)
+{
+    const float a = 2.51;
+    const float b = 0.03;
+    const float c = 2.43;
+    const float d = 0.59;
+    const float e = 0.14;
+    return (x * (a * x + b)) / (x * (c * x + d) + e);
+}
+vec3 Tonemap_Uncharted2(vec3 x)
+{
+    x *= 16.0;
+    const float A = 0.15;
+    const float B = 0.50;
+    const float C = 0.10;
+    const float D = 0.20;
+    const float E = 0.02;
+    const float F = 0.30;
+    
+    return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
+}
+vec3 Tonemap_Unreal(vec3 x)
+{
+    // Unreal 3, Documentation: "Color Grading"
+    // Adapted to be close to Tonemap_ACES, with similar range
+    // Gamma 2.2 correction is baked in, don't use with sRGB conversion!
+    return x / (x + 0.155) * 1.019;
+}
+vec3 Tonemap_tanh(vec3 x)
+{
+    x = clamp(x, -40.0, 40.0);
+    vec3 exp_neg_2x = exp(-2.0 * x);
+    return -1.0 + 2.0 / (1.0 + exp_neg_2x);
+}
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  float d = length(uv)-.4;
+  float glow_d = pow(20./R.y/d,2.);
+
+  float ang = atan(uv.y,uv.x);
+  float a = cos(ang*4.);
+  vec3 col = s1(vec3(3,2,1)+a*2.5+T);
+
+  //https://mini.gmshaders.com/p/func-mix
+  // float gray = dot(col, vec3(0.2126, 0.7152, 0.0722));
+  // col = mix(vec3(gray),col, 1.7);
+
+  col *= glow_d;
+
+  col = Tonemap_ACES(col);
+  // col = Tonemap_Uncharted2(col);
+  // col = Tonemap_Unreal(col);
+  // col = Tonemap_tanh(col);
+
+  O.rgb = col;
+}
