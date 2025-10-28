808219a495b6d0b1507c9ea916491769abfe9a16
yufengjie <2457522528@qq.com>
2025-10-28 20:28:00 +0800

    1

---
+++ shaderToy/raymarch参数_i.glsl
@@ -0,0 +1,67 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-3;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  vec3 ro = vec3(0.,0.,-10.);
+
+  vec3 rd = normalize(vec3(uv, 1.));
+
+  float zMax = 50.;
+  float z = .1;
+
+  vec3 col = vec3(0);
+  for(float i=0.;i<100.;i++){
+    vec3 p = ro + rd * z;
+
+    p.xz = rotate(T+i*2.3)*p.xz;
+    p.yz = rotate(T+i*2.3)*p.yz;
+
+    float d = length(p) - 4.;
+    d = abs(d)*.6 + .01;
+
+    d += fbm(p*1.5)*.3;
+
+    col += (1.1+sin(vec3(3,2,1)+dot(p,vec3(1))))/d;
+    
+    if(d<EPSILON || z>zMax) break;
+    z += d;
+  }
+
+  col = tanh(col / 3e2);
+
+  O.rgb = col;
+}
