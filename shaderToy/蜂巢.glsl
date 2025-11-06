0b4b1097b468a77cf1b76ba06050e0e2315c8ab2
yufengjie <2457522528@qq.com>
2025-11-06 11:36:56 +0800

    1

---
+++ shaderToy/蜂巢.glsl
@@ -0,0 +1,88 @@
+
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-3;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+float hash(vec2 p){
+  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
+}
+
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+// https://www.shadertoy.com/view/Xd2GR3
+// { 2d cell id, distance to border, distnace to center )
+vec4 hexagon( vec2 p ) 
+{
+	vec2 q = vec2( p.x*2.0*0.5773503, p.y + p.x*0.5773503 );
+	
+	vec2 pi = floor(q);
+	vec2 pf = fract(q);
+
+	float v = mod(pi.x + pi.y, 3.0);
+
+	float ca = step(1.0,v);
+	float cb = step(2.0,v);
+	vec2  ma = step(pf.xy,pf.yx);
+	
+    // distance to borders
+	float e = dot( ma, 1.0-pf.yx + ca*(pf.x+pf.y-1.0) + cb*(pf.yx-2.0*pf.xy) );
+
+	// distance to center	
+	p = vec2( q.x + floor(0.5+p.y/1.5), 4.0*p.y/3.0 )*0.5 + 0.5;
+	float f = length( (fract(p) - 0.5)*vec2(1.0,0.85) );		
+	
+	return vec4( pi + ca - cb*ma, e, f );
+}
+
+
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  uv *= 10.;
+  vec4 h = hexagon(uv);
+  float n = fbm(vec3(h.xy*1.1,T));
+  vec3 col = s1(vec3(3,2,1)+n*10.);
+  // vec3 col = palette(n);
+  float glow_part = pow(.1/h.w,2.);
+
+  O.rgb = col * glow_part + col*h.z;
+}
