e542818a23b094c8f3f33823c68d9c4a6c564504
yufengjie <2457522528@qq.com>
2025-11-08 20:35:38 +0800

    1

--- shaderToy/0_tempalte_easy.glsl
+++ shaderToy/0_tempalte_easy.glsl
@@ -77,19 +77,6 @@ float fbm(vec3 p){
 }
 
 
-// Hexagonal prism, circumcircle variant
-float fHexagonCircumcircle(vec3 p, vec2 h) {
-	vec3 q = abs(p);
-	return max(q.y - h.y, max(q.x*sqrt(3.)*0.5 + q.z*0.5, q.z) - h.x);
-	//this is mathematically equivalent to this line, but less efficient:
-	//return max(q.y - h.y, max(dot(vec2(cos(PI/3), sin(PI/3)), q.zx), q.z) - h.x);
-}
-
-// Hexagonal prism, incircle variant
-float fHexagonIncircle(vec3 p, vec2 h) {
-	return fHexagonCircumcircle(p, vec2(h.x*sqrt(3.)*0.5, h.y));
-}
-
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
   vec2 uv = (I*2.-R)/R.y;
