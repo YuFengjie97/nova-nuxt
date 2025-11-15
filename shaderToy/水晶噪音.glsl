3d6e31ec6dd9486b68370117d1e0beaef50ca68e
yufengjie <2457522528@qq.com>
2025-11-15 21:44:04 +0800

    1

---
+++ shaderToy/水晶噪音.glsl
@@ -0,0 +1,152 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-3;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+float hash(vec2 p){
+  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
+}
+
+vec2 randomGradient(vec2 p){
+  float a = hash(p)*TAU;
+  return vec2(cos(a), sin(a));
+}
+
+float noise(vec2 p){
+  vec2 i = floor(p);
+  vec2 f = fract(p);
+
+  vec2 g00 = randomGradient(i+vec2(0,0));
+  vec2 g10 = randomGradient(i+vec2(1,0));
+  vec2 g01 = randomGradient(i+vec2(0,1));
+  vec2 g11 = randomGradient(i+vec2(1,1));
+
+  float v00 = dot(g00, f-vec2(0,0));
+  float v10 = dot(g10, f-vec2(1,0));
+  float v01 = dot(g01, f-vec2(0,1));
+  float v11 = dot(g11, f-vec2(1,1));
+
+  vec2 u = smoothstep(0.,1.,f);
+
+  return mix(mix(v00,v10,u.x), mix(v01,v11,u.x), u.y);
+}
+
+float fbm(vec2 p){
+  float amp = 1.;
+  float n = 0.;
+  for(float i =0.;i<6.;i++){
+    n += noise(p)*amp;
+    amp *= .5;
+    p *= 2.;
+  }
+  return n;
+}
+
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+
+// https://iquilezles.org/articles/distfunctions/
+float smin( float d1, float d2, float k )
+{
+    k *= 4.0;
+    float h = max(k-abs(d1-d2),0.0);
+    return min(d1, d2) - h*h*0.25/k;
+}
+
+float smax( float d1, float d2, float k )
+{
+    return -smin(-d1,-d2,k);
+}
+// https://mercury.sexy/hg_sdf
+float vmax(vec3 v) {
+	return max(max(v.x, v.y), v.z);
+}
+float fBoxCheap(vec3 p, vec3 b) { //cheap box
+	return vmax(abs(p) - b);
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  vec3 ro = vec3(0.,0.,-10.);
+
+  vec3 rd = normalize(vec3(uv, 1.));
+
+  float zMax = 30.;
+  float z = .1;
+
+  vec3 col = vec3(0);
+  for(float i=0.;i<100.;i++){
+    vec3 p = ro + rd * z;
+    // p.z += T*2.;
+
+    if(iMouse.z>0.){
+      p.xz *= rotate(m.x);
+      p.yz *= rotate(m.y);
+    }
+
+    float v = 0.;
+    vec3 q = p;
+    // float n = 0.;
+    for(float s=1.;s<5.;s++){
+      q.xz = rotate(.5)*q.xz;
+      v += (asin(sin(q.y*s)) + asin(sin(q.z*s)))/s;
+      // n += abs(dot(cos(q), vec3(.1)));
+    }
+
+    // v = mix(v, n, tanh(sin(T*3.)*3.)*.3+.3);
+    // float d = length(p) - 4.;
+    float d = fBoxCheap(p, vec3(4));
+    // float d = length(p.xy)-5.;
+    // d = max(v, -d);
+    d = smax(d, -v, .3);
+    d = abs(d)+.03;
+    // d = max(0.01, d);
+
+    col += (1.1+sin(vec3(3,2,1)+cos(p*.3)))/d;
+    
+    if(d<EPSILON || z>zMax) break;
+    z += d*.2;
+  }
+  // col *= exp(2e-3*z*z*z);
+  // col *= z*z*z;
+  col = tanh(col / 9e2);
+
+  O.rgb = col;
+}
