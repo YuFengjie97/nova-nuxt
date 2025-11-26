b821733df9d370f9a63b4513f43c06965a1f2343
yufengjie <2457522528@qq.com>
2025-11-26 11:07:31 +0800

    1

---
+++ shaderToy/重复域cos.glsl
@@ -0,0 +1,122 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+
+#define T iTime
+#define PI 3.141596
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-3;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+
+float fbm(vec3 p){
+  float amp = 1.;
+  float fre = 1.;
+  float n = 0.;
+  for(float i =0.;i<4.;i++){
+    n += abs(dot(cos(p), vec3(.1)));
+    amp *= .5;
+    fre *= 2.;
+  }
+  return n;
+}
+float hash13(vec3 p3)
+{
+	p3  = fract(p3 * .1031);
+    p3 += dot(p3, p3.zyx + 31.32);
+    return fract((p3.x + p3.y) * p3.z);
+}
+
+// iq's sdf function
+float sdBoxFrame( vec3 p, vec3 b, float e )
+{
+       p = abs(p  )-b;
+  vec3 q = abs(p+e)-e;
+  return min(min(
+      length(max(vec3(p.x,q.y,q.z),0.0))+min(max(p.x,max(q.y,q.z)),0.0),
+      length(max(vec3(q.x,p.y,q.z),0.0))+min(max(q.x,max(p.y,q.z)),0.0)),
+      length(max(vec3(q.x,q.y,p.z),0.0))+min(max(q.x,max(q.y,p.z)),0.0));
+}
+float smin( float d1, float d2, float k )
+{
+    k *= 4.0;
+    float h = max(k-abs(d1-d2),0.0);
+    return min(d1, d2) - h*h*0.25/k;
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  vec3 ro = vec3(0.,0.,-12.);
+
+  vec3 rd = normalize(vec3(uv, 1.));
+
+  float zMax = 50.;
+  float z = .1;
+
+  vec3 col = vec3(0);
+
+  mat2 mx = rotate(T);
+  mat2 my = rotate(T);
+  if(iMouse.z>0.){
+    mx = rotate(m.x);
+    my = rotate(m.y);
+  }
+
+  for(float i=0.;i<100.;i++){
+    vec3 p = ro + rd * z;
+
+    p.xz *= mx;
+    p.yz *= my;
+    // p.z += T;
+
+    p = abs(p);
+    // p-=vec3(1,2,3.);
+
+    p = clamp(p,0.,PI*2.);
+    vec3 q = cos(p);
+    vec3 id = floor(p/PI);
+
+
+    float d = sdBoxFrame(q, vec3(.7), .04);
+    float n = hash13(id*10.);
+    if(n<.5){
+      float d1 = (length(q) - .7);
+      d = min(d, d1);
+    }
+
+    d = abs(d) + .01;
+    // d = max(0., d);
+
+    col += s1(vec3(3,2,1)+dot(id, vec3(2)))*pow(.2/d,2.);
+    
+    if(d<EPSILON || z>zMax) break;
+    z += d;
+  }
+
+  col = tanh(col / 6e2);
+
+  O.rgb = col;
+}
