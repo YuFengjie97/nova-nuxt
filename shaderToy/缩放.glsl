c02e18770360046771e48027e0a33d71c77cb639
yufengjie <2457522528@qq.com>
2025-12-02 15:52:55 +0800

    1

--- shaderToy/缩放.glsl
+++ shaderToy/缩放.glsl
@@ -1,80 +1,9 @@
-// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
-// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
-// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
-#iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
-
 #define T iTime
 #define PI 3.141596
 #define TAU 6.283185
 #define S smoothstep
 #define s1(v) (sin(v)*.5+.5)
 const float EPSILON = 1e-3;
-
-mat2 rotate(float a){
-  float s = sin(a);
-  float c = cos(a);
-  return mat2(c,-s,s,c);
-}
-
-vec3 palette( in float t )
-{
-  vec3 a = vec3(0.5, 0.5, 0.5);
-  vec3 b = vec3(0.5, 0.5, 0.5);
-  vec3 c = vec3(1.0, 1.0, 1.0);
-  vec3 d = vec3(0.0, 0.1, 0.2);
-  return a + b*cos( 6.283185*(c*t+d) );
-}
-
-float hash(vec2 p){
-  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
-}
-
-vec2 randomGradient(vec2 p){
-  float a = hash(p)*TAU;
-  return vec2(cos(a), sin(a));
-}
-
-float noise(vec2 p){
-  vec2 i = floor(p);
-  vec2 f = fract(p);
-
-  vec2 g00 = randomGradient(i+vec2(0,0));
-  vec2 g10 = randomGradient(i+vec2(1,0));
-  vec2 g01 = randomGradient(i+vec2(0,1));
-  vec2 g11 = randomGradient(i+vec2(1,1));
-
-  float v00 = dot(g00, f-vec2(0,0));
-  float v10 = dot(g10, f-vec2(1,0));
-  float v01 = dot(g01, f-vec2(0,1));
-  float v11 = dot(g11, f-vec2(1,1));
-
-  vec2 u = smoothstep(0.,1.,f);
-
-  return mix(mix(v00,v10,u.x), mix(v01,v11,u.x), u.y);
-}
-
-float fbm(vec2 p){
-  float amp = 1.;
-  float n = 0.;
-  for(float i =0.;i<6.;i++){
-    n += noise(p)*amp;
-    amp *= .5;
-    p *= 2.;
-  }
-  return n;
-}
-
-float fbm(vec3 p){
-  float amp = 1.;
-  float fre = 1.;
-  float n = 0.;
-  for(float i =0.;i<4.;i++){
-    n += abs(dot(cos(p), vec3(.1)));
-    amp *= .5;
-    fre *= 2.;
-  }
-  return n;
-}
 
 float remap01(float v, float vmin, float vmax){
   return clamp(v-vmin,0.,1.)/(vmax-vmin);
@@ -84,12 +13,13 @@ float remap01(float v, float vmin, float vmax){
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
   vec2 uv = (I*2.-R)/R.y;
+  
+  uv *= 5.;
 
-  uv *= 4.;
 
   vec2 m = vec2(cos(T), sin(T))*2.;
   if(iMouse.z>0.){
-    m = (iMouse.xy*2.-R)/R*4.;
+    m = (iMouse.xy*2.-R)/R*5.;
   }
   vec3 col = vec3(0);
 
@@ -100,16 +30,14 @@ void mainImage(out vec4 O, in vec2 I){
 
 
   float md = length(uv - m);
-  float scl = S(.0,2.4,md);
-  // float scl = remap01(md, 0., .2)*.4;
+  float scl = S(.0,2.2,md);
   uv -= m ;
   uv *= scl;
   uv += m;
-  // uv *= length(uv);
 
-  vec2 grid = abs(fract(uv)-.5);
+  vec2 grid = abs(fract(uv*1.)-.5);
   float d = min(grid.x, grid.y);
-  col += S(20./R.y, 0., d);
+  col += S(30./R.y, 0., d);
 
   O.rgb = col;
 }
