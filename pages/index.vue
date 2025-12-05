f7f5ec137f22220745613a251380dd265e05459a
yufengjie <2457522528@qq.com>
2025-12-05 21:23:42 +0800

    1

--- pages/index.vue
+++ pages/index.vue
@@ -114,8 +114,8 @@
     <nuxt-link to="three/plane3">
       plane3 直接使用顶点着色器来顶点偏移
     </nuxt-link>
-    <nuxt-link to="three/plane4">
-      plane4 几何体取样
+    <nuxt-link to="three/sample">
+      sample 几何体取样
     </nuxt-link>
   </div>
 </template>
