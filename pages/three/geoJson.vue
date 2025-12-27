4e3eb4e8e5a3d9213164546ebfca5a95f3dabc88
yufengjie <2457522528@qq.com>
2025-12-27 11:52:10 +0800

    1

--- pages/three/geoJson.vue
+++ pages/three/geoJson.vue
@@ -26,7 +26,7 @@ onMounted(() => {
   if (!containerRef.value)
     return
   scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100)
+  camera = new THREE.PerspectiveCamera(75, 2, 0.1, 1000)
   renderer = new THREE.WebGLRenderer()
   containerRef.value.appendChild(renderer.domElement)
   controls = new OrbitControls(camera, renderer.domElement)
@@ -42,18 +42,20 @@ onMounted(() => {
     const light = new THREE.AmbientLight()
     scene.add(light)
   }
-  {
-    const light = new THREE.PointLight(0xFFFFFF, 20)
-    light.position.y = 3
+  if (false) {
+    const group = new THREE.Group()
+    const light = new THREE.PointLight(0xFFFFFF, 100)
+    light.position.z = 10
     const helper = new THREE.PointLightHelper(light)
-    scene.add(light)
-    scene.add(helper)
-  }
-  {
-    const geo = new THREE.BoxGeometry(1, 1, 1)
-    const mat = new THREE.MeshBasicMaterial({ color: 0xFF0000 })
-    const mesh = new THREE.Mesh(geo, mat)
-    // scene.add(mesh)
+    group.add(light)
+    group.add(helper)
+    scene.add(group)
+
+    loops.push(() => {
+      group.rotation.x += 0.01
+      group.rotation.y += 0.01
+      group.rotation.z += 0.01
+    })
   }
   async function getData() {
     const res = await fetch(`${baseUrl}data/100000.json`)
@@ -63,28 +65,28 @@ onMounted(() => {
   type Coord = [number, number]
   type Coords = Array<Coord>
 
-  function drawProvinceGeo(coords: Coords) {
-    const position: number[] = []
-    const index: number[] = []
+  function drawMesh(coords: Coords, color: THREE.Color) {
+    const position: number[] = [] // 收集的侧面顶点buffer
+    const index: number[] = [] // 侧面三角形
     let ndx = 0
-    const face: number[] = []
-    const zHeight = 1
+    const zHeight = 1 // 侧面高度
     for (let i = 0; i < coords.length - 1; i++) {
       const p_cur = coords[i]
       const p_nex = coords[i + 1]
+
+      // p_cur[0] -= 116.3683244/2
+      // p_cur[1] -= 39.915085/2
+      // p_nex[0] -= 116.3683244/2
+      // p_nex[1] -= 39.915085/2
+
       const p1 = new THREE.Vector3(p_cur[0], p_cur[1], 0)
       const p2 = new THREE.Vector3(p_cur[0], p_cur[1], zHeight)
       const p3 = new THREE.Vector3(p_nex[0], p_nex[1], 0)
       const p4 = new THREE.Vector3(p_nex[0], p_nex[1], zHeight)
       position.push(...p1.toArray(), ...p2.toArray(), ...p3.toArray(), ...p4.toArray())
-      // index.push(0,1,2, 1,2,3)
       index.push(ndx, ndx + 1, ndx + 2, ndx + 3, ndx + 2, ndx + 1)
       ndx += 4
     }
-    const coordsFlat: number[] = []
-    coords.forEach((item) => {
-      coordsFlat.push(...item)
-    })
 
     const shape = new THREE.Shape()
     coords.forEach(([x, y], ndx) => {
@@ -98,40 +100,75 @@ onMounted(() => {
     shape.closePath()
     const faceGeo = new THREE.ShapeGeometry(shape)
     faceGeo.deleteAttribute('uv')
+    faceGeo.deleteAttribute('normal')
     const faceGeo2 = faceGeo.clone()
-    faceGeo2.translate(0, 0, zHeight)
+    faceGeo.translate(0, 0, zHeight)
 
-    console.log(position)
-    console.log(index)
     const geo = new THREE.BufferGeometry()
     geo.setAttribute('position', new THREE.Float32BufferAttribute(position, 3))
     geo.setIndex(index)
-    geo.computeVertexNormals()
+    // geo.computeVertexNormals()
 
     const geoMerge = mergeGeometries([geo, faceGeo, faceGeo2], false)
 
-    geoMerge.center()
-    geoMerge.computeVertexNormals()
-    const mat = new THREE.MeshPhongMaterial({ color: 0x990000, side: THREE.DoubleSide })
+    // geoMerge.computeVertexNormals()
+    const mat = new THREE.MeshBasicMaterial({ color, side: THREE.DoubleSide })
     const mesh = new THREE.Mesh(geoMerge, mat)
+    return mesh
+  }
+  function drawProvince(coordss: Coords[]) {
+    const group = new THREE.Group() // 一个省下的所有mesh
+    coordss.forEach((coords, ndx) => {
+      const col = new THREE.Color()
+      col.setHSL(Math.random(), 0.8, 0.3)
+      const mesh = drawMesh(coords, col)
+      mesh.position.x -= 116.3683244
+      mesh.position.y -= 39.915085
+      group.add(mesh)
+    })
+    return group
+  }
+  function drawChina(res: any) {
+    const group = new THREE.Group()
+    scene.add(group)
+    res.features.forEach((data: any) => {
+      let coordss
+      const geoType = data.geometry.type
+      if (geoType === 'MultiLineString') {
+        return
+      }
+      if (geoType === 'Polygon') {
+        coordss = data.geometry.coordinates
+      }
+      else if (geoType === 'MultiPolygon') {
+        // const arr = []
+        // data.geometry.coordinates.forEach(item => {
+        //   arr.push(...item)
+        // })
+        // coordss = arr
+        coordss = data.geometry.coordinates.flat()
+      }
 
-    {
+      const provinceMesh = drawProvince(coordss)
+      group.add(provinceMesh)
+
+      const zOff = Math.random() * 2 + 1
+      const speed = Math.random() * 10 + 1.0
       loops.push(() => {
-        mesh.rotation.y += 0.01
+        const time = Date.now() * 0.001 * speed
+        provinceMesh.position.z = (zOff + Math.sin(time))
       })
-    }
-
-    mesh.scale.set(0.4, 0.4, 1)
-    scene.add(mesh)
+    })
+    group.rotation.y = Math.PI
+    group.scale.set(0.4, 0.4, 1)
   }
   getData()
     .then((res) => {
       res = JSON.parse(res)
-      console.log(res)
 
       // @ts-ignore
-      const p = res.features[4].geometry.coordinates[0] as Coords
-      drawProvinceGeo(p)
+      // const p = res.features[4].geometry.coordinates as Coords[]
+      drawChina(res)
     })
 
   function animate() {
