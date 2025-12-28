ba3f10c782d7f5c7514be6be45bd66814c069a1b
yufengjie <2457522528@qq.com>
2025-12-28 20:29:44 +0800

    2

---
+++ shaderToy/fract湍流 copy.glsl
@@ -0,0 +1,85 @@
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture1.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
+#iChannel0 "file://D:/workspace/nova-nuxt/public/sound/shaderToy_1.mp3"
+
+
+#define T iTime
+#define PI 3.1415926
+#define TAU 6.283185
+#define S smoothstep
+#define s1(v) (sin(v)*.5+.5)
+const float EPSILON = 1e-3;
+
+mat2 rotate(float a){
+  float s = sin(a);
+  float c = cos(a);
+  return mat2(c,-s,s,c);
+}
+
+vec3 palette( in float t )
+{
+  vec3 a = vec3(0.5, 0.5, 0.5);
+  vec3 b = vec3(0.5, 0.5, 0.5);
+  vec3 c = vec3(1.0, 1.0, 1.0);
+  vec3 d = vec3(0.0, 0.1, 0.2);
+  return a + b*cos( 6.283185*(c*t+d) );
+}
+
+// https://www.shadertoy.com/view/lsKcDD
+mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
+{
+	vec3 cw = normalize(ta-ro);            // 相机前
+	vec3 cp = vec3(sin(cr), cos(cr),0.0);  // 滚角
+	vec3 cu = normalize( cross(cw,cp) );   // 相机右
+	vec3 cv = normalize( cross(cu,cw) );   // 相机上
+  return mat3( cu, cv, cw );
+
+  // vec3 front = normalize(ta - ro);
+  // vec3 up = vec3(0,1,0);
+  // vec3 right = normalize(cross(front, up));
+  // return mat3(right, up, front);
+}
+float sdBoxFrame( vec3 p, vec3 b, float e )
+{
+       p = abs(p  )-b;
+  vec3 q = abs(p+e)-e;
+  return min(min(
+      length(max(vec3(p.x,q.y,q.z),0.0))+min(max(p.x,max(q.y,q.z)),0.0),
+      length(max(vec3(q.x,p.y,q.z),0.0))+min(max(q.x,max(p.y,q.z)),0.0)),
+      length(max(vec3(q.x,q.y,p.z),0.0))+min(max(q.x,max(q.y,p.z)),0.0));
+}
+
+void mainImage(out vec4 O, in vec2 I){
+  vec2 R = iResolution.xy;
+  vec2 uv = (I*2.-R)/R.y;
+  vec2 m = (iMouse.xy*2.-R)/R * PI * 2.;
+
+  O.rgb *= 0.;
+  O.a = 1.;
+
+  vec3 ro = vec3(0.,0.,-10.);
+  vec3 rd = setCamera(ro, vec3(0), 0.) * normalize(vec3(uv, 1.));
+
+  float z = .1;
+
+  vec3 col = vec3(0);
+  vec3 C = vec3(3,2,1);
+
+  uv *= 3.;
+
+  for(float s = 1.;s<20.;s++){
+    uv = abs(uv);
+    uv += abs(fract(uv.yx*s-T*1.)-.5)/s;
+  }
+
+  C += dot(uv*10., vec2(.1))*2.;
+
+  float d = dot(abs(fract(uv)-.5), vec2(.1));
+  col += s1(C) / d;
+
+  col = tanh(col/20.);
+
+  O.rgb = col;
+}
