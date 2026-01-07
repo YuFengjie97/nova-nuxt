708d51f0dbe56534709a0bd79e5af9d8221763e6
yufengjie <2457522528@qq.com>
2026-01-07 14:00:22 +0800

    1

--- pages/three/realisticRender/index.vue
+++ pages/three/realisticRender/index.vue
@@ -1,3 +1,10 @@
+/**
+逼真渲染
+renderer开启tonemapping
+物体环境贴图
+阴影的控制
+*/
+
 <script lang="ts" setup>
 import * as THREE from 'three'
 import { GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js'
@@ -24,7 +31,7 @@ useEffect(async () => {
 
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100)
-  const renderer = new THREE.WebGLRenderer()
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
   const loops: Array<(delta: number, elapsed: number) => void> = []
   const pane: Pane = new Pane({ container: containerRef.value })
   const controls: OrbitControls = new OrbitControls(camera, renderer.domElement)
@@ -54,6 +61,30 @@ useEffect(async () => {
   renderer.shadowMap.enabled = true
   renderer.shadowMap.type = THREE.PCFSoftShadowMap
   controls.enableDamping = true
+
+  renderer.outputColorSpace = THREE.SRGBColorSpace
+  renderer.toneMapping = THREE.NoToneMapping
+  renderer.toneMappingExposure = 2.0
+
+  pane.addBlade({
+    view: 'list',
+    label: 'toneMapping',
+    options: [
+      { text: 'no', value: THREE.NoToneMapping },
+      { text: 'cineon', value: THREE.CineonToneMapping },
+      { text: 'custom', value: THREE.CustomToneMapping },
+      { text: 'linear', value: THREE.LinearToneMapping },
+      { text: 'neutral', value: THREE.NeutralToneMapping },
+      { text: 'reinhard', value: THREE.ReinhardToneMapping },
+      { text: 'aces', value: THREE.ACESFilmicToneMapping },
+    ],
+    value: THREE.NoToneMapping,
+  })
+  // @ts-ignore
+    .on('change', (ev) => {
+      renderer.toneMapping = ev.value
+    })
+  pane.addBinding(renderer, 'toneMappingExposure', { min: 0.1, max: 10, step: 0.1 })
 
   handleResize()
   window.addEventListener('resize', handleResize)
@@ -98,8 +129,14 @@ useEffect(async () => {
     light.castShadow = true
     // light.intensity = 100
     light.position.set(4, 2, 0)
-    light.shadow.mapSize.width = 1024
-    light.shadow.mapSize.height = 1024
+    light.shadow.mapSize.set(1024, 1024)
+    /*
+    光滑曲面的几何体,同时设置castShadow 和 receiveShadow,
+    该面即会产生阴影,也会接受阴影,又因为是光滑曲面的原因,会在表面产生伪影(一小块一小块,又或者是一行又一行)
+    也就是阴影和mesh表面重叠
+    这个属性相当于将阴影沿着法线做细微偏移,让阴影贴图不再跟表面重叠,或者说是阴影不会在该表面产生阴影
+    */
+    light.shadow.normalBias = 0.005
     scene.add(light)
 
     const shadowCamera = light.shadow.camera
@@ -126,33 +163,46 @@ useEffect(async () => {
     scene.add(mesh)
   }
 
+  const cubeTexture = cubeTexLoader.load([
+    'photo_studio_loft_hall_1k/px.png',
+    'photo_studio_loft_hall_1k/nx.png',
+    'photo_studio_loft_hall_1k/py.png',
+    'photo_studio_loft_hall_1k/ny.png',
+    'photo_studio_loft_hall_1k/pz.png',
+    'photo_studio_loft_hall_1k/nz.png',
+  ])
+  scene.background = cubeTexture
+
   {
     const model = await gltfloader.loadAsync(`${baseUrl}model/2010_subaru_impreza_wrx_sti/scene.gltf`).then(res => res.scene)
-    // model.castShadow = true
-    model.traverse((obj) => {
-      if (obj instanceof THREE.Mesh) {
-        obj.castShadow = true
-      }
-    })
     scene.add(model)
+
+    const params = {
+      intensity: 1,
+      showEnvMap: true,
+    }
+
+    function updateModelMat() {
+      const { intensity, showEnvMap } = params
+      model.traverse((obj) => {
+        if (obj instanceof THREE.Mesh && obj.material instanceof THREE.MeshStandardMaterial) {
+          obj.castShadow = true
+          obj.receiveShadow = true
+          obj.material.envMap = showEnvMap ? cubeTexture : null
+          obj.material.envMapIntensity = intensity
+        }
+      })
+    }
+    updateModelMat()
+
+    pane.addBinding(params, 'intensity', { min: 0.1, max: 10, step: 0.1 }).on('change', ev => updateModelMat())
+    pane.addBinding(params, 'showEnvMap').on('change', ev => updateModelMat())
 
     // const geo = new THREE.SphereGeometry(2)
     // const mat = new THREE.MeshStandardMaterial()
     // const mesh = new THREE.Mesh(geo, mat)
     // scene.add(mesh)
     // mesh.castShadow = true
-  }
-
-  {
-    const cubeTexture = cubeTexLoader.load([
-      'photo_studio_loft_hall_1k/px.png',
-      'photo_studio_loft_hall_1k/nx.png',
-      'photo_studio_loft_hall_1k/py.png',
-      'photo_studio_loft_hall_1k/ny.png',
-      'photo_studio_loft_hall_1k/pz.png',
-      'photo_studio_loft_hall_1k/nz.png',
-    ])
-    scene.background = cubeTexture
   }
 
   return () => {
