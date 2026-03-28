2eeb46d93a24cecca0290940972670a4d3056644
yufengjie <2457522528@qq.com>
2026-03-28 10:59:04 +0800

    1

--- shaderToy/0_tempalte_easy copy 6.glsl
+++ shaderToy/0_tempalte_easy copy 6.glsl
@@ -2,7 +2,7 @@
 // #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture2.jpg"
 // #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture3.jpg"
 // #iChannel0 "file://D:/workspace/nova-nuxt/public/img/noise/shaderToy/texture4.jpg"
-#iChannel0 "file://D:/workspace/nova-nuxt/public/sound/shaderToy_1.mp3"
+// #iChannel0 "file://D:/workspace/nova-nuxt/public/sound/shaderToy_1.mp3"
 
 
 #define T iTime
@@ -10,6 +10,9 @@
 #define TAU 6.283185
 #define S smoothstep
 #define s1(v) (sin(v)*.5+.5)
+#define P(z) vec3(cos((z)*.1)*4.,sin((z)*.1)*4.,(z))
+#define t_path T*5.
+
 const float EPSILON = 1e-3;
 
 mat2 rotate(float a){
@@ -27,56 +30,7 @@ vec3 palette( in float t )
   return a + b*cos( 6.283185*(c*t+d) );
 }
 
-float hash(vec2 p){
-  return fract(sin(dot(p, vec2(456.456,7897.7536)))*741.25639);
-}
 
-vec2 randomGradient(vec2 p){
-  float a = hash(p)*TAU;
-  return vec2(cos(a), sin(a));
-}
-
-float noise(vec2 p){
-  vec2 i = floor(p);
-  vec2 f = fract(p);
-
-  vec2 g00 = randomGradient(i+vec2(0,0));
-  vec2 g10 = randomGradient(i+vec2(1,0));
-  vec2 g01 = randomGradient(i+vec2(0,1));
-  vec2 g11 = randomGradient(i+vec2(1,1));
-
-  float v00 = dot(g00, f-vec2(0,0));
-  float v10 = dot(g10, f-vec2(1,0));
-  float v01 = dot(g01, f-vec2(0,1));
-  float v11 = dot(g11, f-vec2(1,1));
-
-  vec2 u = smoothstep(0.,1.,f);
-
-  return mix(mix(v00,v10,u.x), mix(v01,v11,u.x), u.y);
-}
-
-float fbm(vec2 p){
-  float amp = 1.;
-  float n = 0.;
-  for(float i =0.;i<6.;i++){
-    n += noise(p)*amp;
-    amp *= .5;
-    p *= 2.;
-  }
-  return n;
-}
-
-float fbm(vec3 p){
-  float amp = 1.;
-  float fre = 1.;
-  float n = 0.;
-  for(float i =0.;i<4.;i++){
-    n += abs(dot(cos(p*fre), vec3(.1)));
-    amp *= .5;
-    fre *= 2.;
-  }
-  return n;
-}
 // https://www.shadertoy.com/view/lsKcDD
 mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
 {
@@ -101,6 +55,42 @@ float sdBoxFrame( vec3 p, vec3 b, float e )
       length(max(vec3(q.x,q.y,p.z),0.0))+min(max(q.x,max(q.y,p.z)),0.0));
 }
 
+float noise(vec3 p){
+  // p*=vec3(.1,.1,2.2);
+  // float n1 = dot(cos(p+tanh(p*10.)), vec3(.1));
+  float n1 = dot(cos(p+sin(p*.3)*.5), vec3(.1));
+  float n2 = dot(cos(p), vec3(.1));
+  return mix(n1,n2,s1(T));
+}
+
+vec3 C = vec3(10,2,1);
+vec3 RO;
+float fbm(vec3 p){
+  float amp = 1.;
+  float n = 0.;
+
+  for(float i=0.;i<4.;i++){
+    p.xz *= rotate(.2);
+    p += sin(p.zxy+T*i*2.)*amp;
+    n += abs(noise(p))*amp;
+    amp *= .5;
+    p *= 2.;
+    C += amp*.1;
+  }
+  return n;
+}
+
+vec3 col = vec3(0);
+float map(vec3 p){
+
+  float d = fbm(p);
+  float d1 = length(p - RO)-4.;
+  d = max(d, -d1);
+  col += s1(C+T)/(max(d, .01));
+
+  return d;
+}
+
 void mainImage(out vec4 O, in vec2 I){
   vec2 R = iResolution.xy;
   vec2 uv = (I*2.-R)/R.y;
@@ -109,39 +99,21 @@ void mainImage(out vec4 O, in vec2 I){
   O.rgb *= 0.;
   O.a = 1.;
 
-  vec3 ro = vec3(0.,10.,-10.);
-  vec3 rd = setCamera(ro, vec3(0), 0.) * normalize(vec3(uv, 1.));
+  vec3 ro = P(t_path);
+  RO = ro;
+  vec3 rd = setCamera(ro, P(t_path+1.), s1(T*.5)) * normalize(vec3(uv, 1.));
 
   float z = .1;
 
-  mat2 mx = rotate(T);
-  mat2 my = rotate(T);
-  if(iMouse.z>0.){
-    mx = rotate(m.x);
-    my = rotate(m.y);
-  }
-
-  vec3 col = vec3(0);
-  vec3 C = vec3(3,2,1);
   float i=0.;
   while(i++<80.){
     vec3 p = ro + rd * z;
 
-    p.xz *= mx;
-    p.yz *= my;
-
-
-    float d = sdBoxFrame(p, vec3(4.), .5);
-    d = abs(d)*.3 + .01;
-
-    if(d<EPSILON) break;
-    
-    col += s1(C+i*.1-T)/d;
-    
+    float d = map(p);
     z += d;
   }
 
-  col = tanh(col / 2e3);
 
-  O.rgb = col;
+  O.rgb += col;
+  O.rgb = tanh(O.rgb/1e3);
 }
